620501046cca69196217c235124911a5759cf8a6
vaksis-kun <62132561+vaksis@users.noreply.github.com>
2021-04-15 15:42:13 +0300

    Delete discordBot_2 directory

--- discordBot_2/commands/github.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'github',
-    description: "gives vaksis's github profile",
-    execute(message, args){
-        message.channel.send('https://github.com/vaksis');
-    }
-}
